0b0fb4dc61e5aaf9b4de05c4c5d591ef1b4fab9c
pranavmanduri23-hash <pranavmanduri23@gmail.com>
2026-05-01 12:58:25 +0530

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,18 +2,23 @@ import { Events } from "discord.js";
 import { logger, startupLog } from "../utils/logger.js";
 import config from "../config/application.js";
 import { reconcileReactionRoleMessages } from "../services/reactionRoleService.js";
+import { cacheGuildInvites } from "../services/inviteService.js"; // ← ADD
 
 export default {
   name: Events.ClientReady,
   once: true,
-
   async execute(client) {
     try {
       client.user.setPresence(config.bot.presence);
-
       startupLog(`Ready! Logged in as ${client.user.tag}`);
       startupLog(`Serving ${client.guilds.cache.size} guild(s)`);
       startupLog(`Loaded ${client.commands.size} commands`);
+
+      // ← ADD: Cache invites for all guilds
+      for (const [, guild] of client.guilds.cache) {
+        await cacheGuildInvites(guild);
+      }
+      startupLog(`Invite cache loaded for ${client.guilds.cache.size} guild(s)`);
 
       const reconciliationSummary = await reconcileReactionRoleMessages(client);
       startupLog(
@@ -24,5 +29,3 @@ export default {
     }
   },
 };
-
-
